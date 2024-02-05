fb081e5a2e5470f561e74c5e1b600fd0ff95e735
tonyshu <tonynshu@gmail.com>
2024-02-05 08:14:30 +0800

    finished

--- src/Dog.java
+++ src/Dog.java
@@ -1,2 +1,67 @@
 public class Dog {
+    //parameters
+    private int age;
+    private String name;
+    private String color;
+    private Owner owner;
+    private int birthday;
+    //constructor
+    public Dog(int age, String name, String color, Owner owner){
+        this.age = age;
+        this.name = name;
+        this.color = color;
+        this.owner = owner;
+    }
+
+    //getters
+
+    public int getBirthday() {
+        return birthday;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    //setters
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public void setBirthday(int birthday) {
+        this.birthday = birthday;
+    }
+
+    private void addOneAge(){
+        this.age++;
+    }
+    //The dog is named Marlo and is 5 years old. This dog is black.
+    @Override
+    public String toString() {
+        return "The dog is named " + this.name + " and is " + age + " years old. This dog is " + color + ".";
+    }
 }
--- src/DogMaker.java
+++ src/DogMaker.java
@@ -1,8 +1,15 @@
 public class DogMaker {
   
   public static void main(String[] args) {
-    
-    
+    Owner owner = new Owner("John", "Wick", "0800322520");
+    //i am confused because the readme only gives three parameters for the dog, but the constructor it told you to make earlier has 4, so this is my compromise
+    Dog dog = new Dog(2, "Jill", "black and tan", owner);
+    dog.setName("Jessie");
+    System.out.println(dog.getAge());
+    //i'm not sure what the birthday method the readme is referring to so i added a parameter to the dog class
+    int age = dog.getBirthday();
+    System.out.println(dog.getAge());
+    System.out.println(dog);
   }
   
 }
